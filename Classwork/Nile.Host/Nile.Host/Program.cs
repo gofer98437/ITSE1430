bee1c71ce3bbcd73510f4dbad55edbe226aa87f6
chas <chas.pena@my.tccd.edu>
2018-01-24 21:43:08 -0600

    Todays lecture

---
+++ Classwork/Nile.Host/Nile.Host/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+namespace Nile.Host
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+
+
+   
+        }
+
+        static void PlayingWithPrimitives()
+        {
+            decimal unitPrice = 10.5m;
+
+            //real declaration
+            System.Decimal unitPrice2 = 10.5m;
+
+            //current time
+            System.DateTime now = DateTime.Now;
+
+            ArrayList items;
+        
+        }
+
+        static void PlayingWithVariables()
+        {
+
+            int hours = 8;
+            double rate = 35.7;
+
+            int hours2 = hours;
+
+            string firstName, lastName;
+
+            rate = 50;
+
+            firstName = "dogcat";
+
+            firstName = lastName = "what thats no name!";
+
+            double ceiling = Math.Ceiling(rate);
+            double floor = ceiling;
+
+        }
+
+    }
+}
