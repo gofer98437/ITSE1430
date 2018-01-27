45481d2edec9c6503767a41444603d9639f2b34b
Chas <chas.pena@my.tccd.edu>
2018-01-27 12:57:15 -0600

    Playing with menu structure and syntax differences bewteen c++.

--- Lab1/ChasPena.MovieLib.Host/Program.cs
+++ Lab1/ChasPena.MovieLib.Host/Program.cs
@@ -10,9 +10,53 @@ namespace ChasPena.MovieLib.Host
     {
         static void Main( string[] args )
         {
+            int choice = 0;
+            bool mainMenu = false;
+            string filmTitle = "None";
+
+            List<string> filmList = new List<string>{"None"};
+
+
+            Console.WriteLine("\nPlease enter the name of the Film: ");
+            filmTitle = Console.ReadLine();
 
 
 
+            while (mainMenu == true)
+            {
+
+                switch (choice)
+                {
+/*
+Console applications typically have a menu to allow the user to interact with the system. Our application will allow the following options.
+
+List the movies
+Add a new movie
+Remove an existing movie
+Exit the application
+Create a menu to display the available options. 
+Until the user selects the option to exit, keep displaying the menu and performing the requested action.
+*/
+                    case 1:
+                        {
+                            foreach(string Str in filmList)
+                            {
+                                Console.WriteLine("Just testing");
+                                return;
+                            }
+                            break;
+                        }
+                    
+                    case 2:
+                        {
+                            break;
+                        }
+                    default:
+                    break;
+                }
+
+
+            }
         }
     }
 }
