b95a98b654d8a8fcba5baefcee915beb3e59f2c0
chas <chas.pena@my.tccd.edu>
2018-01-29 21:37:38 -0600

    Class notes

--- Classwork/Nile.Host/Nile.Host/Program.cs
+++ Classwork/Nile.Host/Nile.Host/Program.cs
@@ -7,10 +7,138 @@ namespace Nile.Host
     {
         static void Main( string[] args )
         {
+            bool quit = false;
+            while(!quit)
+            {
+                //display
+                char choice = DisplayMenu();
 
-
-   
+                //process
+                switch (choice)
+                {
+                    case 'L': ListProducts();
+                    break;
+                    case 'A': AddProduct();
+                    break;
+                    case 'Q': quit = true;
+                    break;
+                };
+            };
         }
+
+        static void AddProduct()
+        {
+            _name = ReadString("Enter name: ", true);
+
+            _price = ReadDecimal("Enter price: ", 0);
+
+            _description = ReadString("Enter optional description", false);
+        }
+
+
+        private static decimal ReadDecimal( string message, decimal minValue )
+        {
+            do
+            {
+                Console.Write(message);
+                string value = Console.ReadLine();
+
+                if(Decimal.TryParse(value, out decimal result));
+                {
+                    if (result >= minValue)
+                        return result;
+
+                }
+
+
+                Console.WriteLine("Value must be >= {0}" + minValue);
+            } while (true);
+        }
+
+
+        private static string ReadString(string message, bool isRequired)
+        {
+            do
+            {
+                Console.Write(message);
+                string value = Console.ReadLine();
+                if (!isRequired || value != "")
+                    return value;
+
+                Console.WriteLine("Value is Required");
+            } while (true);
+        }
+
+        static void ListProducts()
+        {
+            if (_name != null && _name != "")
+            {
+                Console.WriteLine(_name);
+                Console.WriteLine(_price);
+                Console.WriteLine(_description);
+            } else
+                Console.WriteLine("No products");
+        }
+
+        //product data
+        static string _name;
+        static decimal _price;
+        static string _description;
+
+
+        private static char DisplayMenu()
+        {
+            do
+            {
+                Console.WriteLine("L)ist product\n" +
+                    "A)dd product\n" +
+                    "Q)uit\n");
+
+                string input = Console.ReadLine();
+
+                if (input == "L")
+                    return input[0];
+                else if (input == "A")
+                    return input[0];
+                else if (input == "Q")
+                    return input[0];
+                
+                Console.Write("\nPlease choose a valid option");
+
+            } while (true);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         static void PlayingWithPrimitives()
         {
