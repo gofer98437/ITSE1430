1d0a391b8110291e2a36ffd5428bec8fed3e0df8
Chas <chas.pena@my.tccd.edu>
2018-02-25 21:28:42 -0600

    Project 2. Start

---
+++ Labs/ChasPena.MovieLib/ChasPena.MovieLib/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChasPena.MovieLib
+{
+    /// <summary>
+    ///                             Represents a Movie
+    /// </summary>
+    public class Movie
+    {
+        string title = "";
+        
+
+
+    }
+}
